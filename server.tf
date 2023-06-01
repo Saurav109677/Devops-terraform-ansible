31aa7275968b41c3ec2bc86397339731c4385562
Saurav109677 <34793927+Saurav109677@users.noreply.github.com>
2023-06-01 16:18:07 +0530

    ami chagne acc to region

--- server.tf
+++ server.tf
@@ -11,7 +11,7 @@ provider "aws" {
 region = "us-west-2"
 }
 resource "aws_instance" "myawsserver" {
-  ami = "ami-0b0dcb5067f052a63"
+  ami = "ami-0ab193018f3e9351b"
   instance_type = "t2.micro"
   key_name = "saurav-pub"
 
